c5c4ed9b57445f584aceb63eaccbb8d0c4826ae1
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-03-04 12:49:26 -0800

    update

--- pytest-tut/extract_position.py
+++ pytest-tut/extract_position.py
@@ -14,12 +14,6 @@ def extract_position(line):
                 pos = None
     return pos
 
-# if __name__ == "__main__":
-#     result1 = extract_position('|error| numerical calculations could not converge.')
-#     print(result1)
-#     result2 = extract_position('|update| the positron location in the particle accelerator is x:21.432')
-#     print(result2)
-
 @pytest.fixture
 def average():
     return "|update| the positron location in the particle accelerator is x:21.432"
